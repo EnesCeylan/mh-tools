81c0114b9e62a0309b9da665a5cc873eebe341eb
Enes <aenesceylan98@gmail.com>
2021-12-29 19:07:50 +0300

    initial commit for HeroList component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import CubeOfTruth from './components/Cube/CubeOfTruth';
 import Hero from './components/Hero/Hero';
+import HeroList from './components/HeroList/HeroList';
 
 function App() {
   return (
     <div className='App'>
       {/* <CubeOfTruth /> */}
-      <Hero />
+      {/* <Hero /> */}
+      <HeroList />
     </div>
   );
 }
