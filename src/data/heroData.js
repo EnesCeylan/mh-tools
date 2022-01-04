c0297a7039c8ee22fb66c7a6a1ecdbb9fa11f405
Kory Nguyen <korynguyen715@gmail.com>
2022-01-04 11:24:22 -0600

    Hero Guide

--- src/data/heroData.js
+++ src/data/heroData.js
@@ -5,37 +5,67 @@ const heroData = {
     rarity: 'UR',
     faction: ['Luminarch', 'Shadowarch'],
     role: 'Fighter',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Air'],
+    artifacts: ['Yasakani Magatama'],
+    divinityNotables: ['New Moon', 'Full Moon', 'Aurora'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
 
   Iset: {
     rarity: 'UR',
     faction: ['Luminarch', 'Verdian'],
     role: 'Support',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Dark', 'Forest', 'Air'],
+    artifacts: ['Excalibur'],
+    divinityNotables: ['Inspiration', 'Death Waiver', 'Excitement', 'Collective Fervor'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   Typhon: {
     rarity: 'UR',
     faction: ['Guardian', 'Verdian'],
     role: 'Tank',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Earth', 'Wind'],
+    artifacts: ['Shield'],
+    divinityNotables: ['Night', 'Excitement'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   Lilith: {
     rarity: 'UR',
     faction: ['Shadowarch', 'Guardian'],
     role: 'Mage',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Thunder', 'Fire'],
+    artifacts: ['Sudarshana Chakra'],
+    divinityNotables: ['Culmination', 'Death Waiver'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   Nuwa: {
     rarity: 'UR',
     faction: ['Guardian', 'Verdian'],
     role: 'Support',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Air', 'Forest'],
+    artifacts: ['Yasakani Magatama'],
+    divinityNotables: ['Aurora', 'Full Moon', 'Transcend', 'Panacea'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   // ----------------------------------- SSR -----------------------------------
   // ------------------------- Luminarch -------------------------
@@ -43,172 +73,316 @@ const heroData = {
     rarity: 'SSR',
     faction: ['Luminarch'],
     role: 'Mage',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Air', 'Thunder', 'Fire'],
+    artifacts: ['Yasakani Magatama', 'Sudarshana Chakra', 'Staff of Sharur'],
+    divinityNotables: ['Death Waiver', 'Spotlight'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   Athena: {
     rarity: 'SSR',
     faction: ['Luminarch'],
     role: 'Tank',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Air', 'Water', 'Earth'],
+    artifacts: ['Yasakani Magatama', 'Aegist Shield', 'Excalibur'],
+    divinityNotables: ['Full Moon', 'New Moon', 'Recovery'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   Susanoo: {
     rarity: 'SSR',
     faction: ['Luminarch'],
     role: 'Fighter',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Water', 'Air', 'Fire'],
+    artifacts: ['Staff of Sharur', 'Sudarshana Chakra'],
+    divinityNotables: ['Death Waiver', 'Savvy', 'Transcend', 'Corrosion'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   Artemis: {
     rarity: 'SSR',
     faction: ['Luminarch'],
     role: 'Fighter',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Fire', 'Air', 'Thunder'],
+    artifacts: ['Yasakani Magatama', 'Staff of Sharur', 'Axe of Pangu'],
+    divinityNotables: ['Spotlight', 'Aurora', 'Transcend', 'Death Waiver'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   Dionysus: {
     rarity: 'SSR',
     faction: ['Luminarch'],
     role: 'Support',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Forest', 'Air', 'Dark'],
+    artifacts: ['Yasakani Magatama'],
+    divinityNotables: ['New Moon'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   Thor: {
     rarity: 'SSR',
     faction: ['Luminarch'],
     role: 'Tank',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Air', 'Earth'],
+    artifacts: ['Yasakani Magatama', 'Aegis Shield'],
+    divinityNotables: ['Phantom', 'Embroilment'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   // ------------------------- Shadowarch -------------------------
   Persephone: {
     rarity: 'SSR',
     faction: ['Shadowarch'],
     role: 'Mage',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Dark', 'Forest', 'Air'],
+    artifacts: ['Yasakani Magatama', 'Staff of Sharur'],
+    divinityNotables: ['Excitement', 'Collective Fervor'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   Izanami: {
     rarity: 'SSR',
     faction: ['Shadowarch'],
     role: 'Mage',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Air', 'Dark', 'Fire'],
+    artifacts: ['Yasakani Magatama', 'Sudarshana Chakra'],
+    divinityNotables: ['Savvy', 'Waterdrop', 'Engulf', 'Phantom'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   Hades: {
     rarity: 'SSR',
     faction: ['Shadowarch'],
     role: 'Tank',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Water', 'Earth'],
+    artifacts: ['Aegis Shield', 'Yasakani Magatama'],
+    divinityNotables: ['Recovery', 'Waterdrop', 'Tenacious', 'Phantom'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   Medusa: {
     rarity: 'SSR',
     faction: ['Shadowarch'],
     role: 'Fighter',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Air', 'Fire', 'Thunder'],
+    artifacts: ['Axe of Pangu', 'Yasakani Magatama'],
+    divinityNotables: ['Wrath', 'Prevail', 'Transcend'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   Anubis: {
     rarity: 'SSR',
     faction: ['Shadowarch'],
     role: 'Tank',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Air', 'Earth'],
+    artifacts: ['Aegis Shield', 'Scarab Badge', 'Yasakani Magatama'],
+    divinityNotables: ['New Moon', 'Phantom', 'Recovery', 'Resonance'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   Hela: {
     rarity: 'SSR',
     faction: ['Shadowarch'],
     role: 'Support',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Air', 'Fire', 'Water'],
+    artifacts: ['Yasakani Magatama'],
+    divinityNotables: ['Engulf', 'Prevail'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   // ------------------------- Guardian -------------------------
   Hercules: {
     rarity: 'SSR',
     faction: ['Guardian'],
     role: 'Tank',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Earth', 'Air'],
+    artifacts: ['Aegis Shield', 'Excalibur', 'Yasakani Magatama'],
+    divinityNotables: ['Panacea', 'Embroilment'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   'Lu Bu': {
     rarity: 'SSR',
     faction: ['Guardian'],
     role: 'Fighter',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Thunder', 'Fire', 'Air'],
+    artifacts: ['Staff of Sharur', 'Yasakani Magatama'],
+    divinityNotables: ['Death Waiver', 'Engulf', 'Wrath', 'Phantom'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   'Ganjiang & Moye': {
     rarity: 'SSR',
     faction: ['Guardian'],
     role: 'Mage',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Thunder'],
+    artifacts: ['Axe'],
+    divinityNotables: ['Death Waiver', 'Supernova'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   'Joan of Arc': {
     rarity: 'SSR',
     faction: ['Guardian'],
     role: 'Support',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Air', 'Water', 'Forest'],
+    artifacts: ['Yasakani Magatama'],
+    divinityNotables: ['Full Moon', 'New Moon', 'Aurora'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   Cleopatra: {
     rarity: 'SSR',
     faction: ['Guardian'],
     role: 'Mage',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Air', 'Fire'],
+    artifacts: ['Yasakani Magatama', 'Staff of Sharur'],
+    divinityNotables: ['Torment', 'Prevail', 'Supernova'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   Cassandra: {
     rarity: 'SSR',
     faction: ['Guardian'],
     role: 'Support',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Water'],
+    artifacts: ['Excalibur'],
+    divinityNotables: ['Full Moon', 'New Moon', 'Purify', 'Inspiration'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   // ------------------------- Verdian -------------------------
   Nagakanya: {
     rarity: 'SSR',
     faction: ['Verdian'],
     role: 'Support',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Air', 'Water'],
+    artifacts: ['Yasakani Magatama', 'Excalibur'],
+    divinityNotables: ['New Moon', 'Waterdrop', 'Self-Hypnosis'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   'Tamamo no Mae': {
     rarity: 'SSR',
     faction: ['Verdian'],
     role: 'Mage',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Lightning', 'Fire', 'Air'],
+    artifacts: ['Sudarshana Chakra', 'Yasakani Magatama'],
+    divinityNotables: ['Savvy', 'Supernova', 'Aurora'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   Idun: {
     rarity: 'SSR',
     faction: ['Verdian'],
     role: 'Support',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Air', 'Water'],
+    artifacts: ['Yasakani Magatama', 'Excalibur'],
+    divinityNotables: ['New Moon', 'Full Moon'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   Gaia: {
     rarity: 'SSR',
     faction: ['Verdian'],
     role: 'Tank',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Light', 'Earth'],
+    artifacts: ['Aegis Shield', 'Excalibur'],
+    divinityNotables: ['Resonance', 'Recovery'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   Oberon: {
     rarity: 'SSR',
     faction: ['Verdian'],
     role: 'Fighter',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Air'],
+    artifacts: ['Yasakani Magatama'],
+    divinityNotables: ['Spotlight', 'Transcend', 'Full Moon', 'Prevail'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
   Flora: {
     rarity: 'SSR',
     faction: ['Verdian'],
     role: 'Mage',
+<<<<<<< HEAD
     runes: '',
     artifacts: '',
+=======
+    runes: ['Fire', 'Forest'],
+    artifacts: ['Yasakani Magatama', 'Sudarshana Chakra'],
+    divinityNotables: ['Spotlight', 'Maestro', 'Aurora', 'Torment'],
+>>>>>>> 4238fb6 (Hero Guide)
   },
 };
 
@@ -219,8 +393,8 @@ export default heroData;
 //     rarity: 'SSR',
 //     faction: [''],
 //     role: '',
-//     runes: '',
-//     artifacts: '',
+//     runes: ['Air', 'Dark', 'Fire'],
+//     artifacts: ['Yasakani Magatama', 'Staff of Sharur', 'Axe of Pangu'],
 //     divinityNotables: ''
 //   },
 // };
