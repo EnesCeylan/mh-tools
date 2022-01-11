6f68fd81eba9c3e8e208a5e9a7627cbf903fae14
Enes <aenesceylan98@gmail.com>
2022-01-11 22:36:55 +0300

    some UI improvements and fixes

--- src/components/Hero/HeroPage.js
+++ src/components/Hero/HeroPage.js
@@ -1,20 +1,20 @@
 import { useParams } from 'react-router-dom';
 import './HeroPage.css';
 import HeroInfoContainer from './HeroInfoContainer';
+import { useWindowSize } from 'react-use';
+
 function Hero({ setShowMenu }) {
+  const { width, height } = useWindowSize();
+
   const { heroName } = useParams();
-  const hero = heroName.replace(/-/g, ' ')
+  const hero = heroName.replace(/-/g, ' ');
+
   return (
-    <section className='hero-container' onClick={() => setShowMenu(false)}>
+    <section className={width < 1440 ? 'hero-container' : 'hero-container divided'} onClick={() => setShowMenu(false)}>
       <img
-        src={
-          process.env.PUBLIC_URL +
-          '/assets/illustration/' +
-          hero +
-          '.png'
-        }
+        src={process.env.PUBLIC_URL + '/assets/illustration/' + hero + '.png'}
         alt={hero}
-        className={"illustration " + heroName}
+        className={'illustration ' + heroName}
       />
       <HeroInfoContainer heroName={heroName} />
     </section>
--- src/components/Hero/IW/IWContainer.js
+++ src/components/Hero/IW/IWContainer.js
@@ -1,68 +1,57 @@
-import IWDescription from "./IWDescription";
-import IWStat from "./IWStat";
-import { useState } from "react";
-import './IWPage.css'
+import IWDescription from './IWDescription';
+import IWStat from './IWStat';
+import { useState } from 'react';
+import './IWPage.css';
 
 const IWContainer = ({ hero }) => {
-    const IWtype = ['Description', 'Stat'];
-    const [currentType, setCurrentType] = useState('Description')
-    const [isIW, setIsIW] = useState(true)
-    const [isStat, setIsStat] = useState(false)
-    function setState(type, isIWval, isStatval) {
-        setCurrentType(type)
-        setIsIW(isIWval)
-        setIsStat(isStatval)
-    }
+  const IWtype = ['Description', 'Stat'];
+  const [currentType, setCurrentType] = useState('Description');
+  const [isIW, setIsIW] = useState(true);
+  const [isStat, setIsStat] = useState(false);
+  function setState(type, isIWval, isStatval) {
+    setCurrentType(type);
+    setIsIW(isIWval);
+    setIsStat(isStatval);
+  }
 
-    return (
-        <div>
-            <div className="weapon-button-container">
-                {IWtype.map((type, index) => {
-                    if (type === 'Description')
-                        return (
-                            <img
-                                src={
-                                    type === currentType
-                                        ? process.env.PUBLIC_URL +
-                                        '/assets/hero/weapon-skill-icons/' +
-                                        hero +
-                                        '.png'
-                                        : process.env.PUBLIC_URL +
-                                        '/assets/hero/weapon-skill-icons/inactive/' +
-                                        hero +
-                                        '.png'
-                                }
-                                alt={hero + "_IW"}
-                                className="IW-button"
-                                onClick={() => setState(type, true, false)}
-                                key={index}
-                            >
-                            </img>
-                        )
-                    else
-                        return (
-                            <img
-                                src={
-                                    type === currentType
-                                        ? process.env.PUBLIC_URL +
-                                        '/assets/hero/weapon-skill-icons/IW_stat.png'
-                                        : process.env.PUBLIC_URL +
-                                        '/assets/hero/weapon-skill-icons/inactive/IW_stat.png'
-                                }
-                                alt="IW_Stat"
-                                className="IW-button"
-                                onClick={() => setState(type, false, true)}
-                                key={index}
-                            >
-                            </img>
-                        )
-                })}
-            </div>
-            {currentType === 'Description' && <IWDescription hero={hero.replace(/-/g, ' ')} />}
-            {currentType === 'Stat' && <IWStat />}
-        </div>
-    );
-}
+  return (
+    <div>
+      <div className='weapon-button-container'>
+        {IWtype.map((type, index) => {
+          if (type === 'Description')
+            return (
+              <img
+                src={
+                  type === currentType
+                    ? process.env.PUBLIC_URL + '/assets/hero/weapon-skill-icons/' + hero.replace(/ /g, '_') + '.png'
+                    : process.env.PUBLIC_URL + '/assets/hero/weapon-skill-icons/inactive/' + hero + '.png'
+                }
+                alt={hero + '_IW'}
+                className='IW-button'
+                onClick={() => setState(type, true, false)}
+                key={index}
+              ></img>
+            );
+          else
+            return (
+              <img
+                src={
+                  type === currentType
+                    ? process.env.PUBLIC_URL + '/assets/hero/weapon-skill-icons/IW_stat.png'
+                    : process.env.PUBLIC_URL + '/assets/hero/weapon-skill-icons/inactive/IW_stat.png'
+                }
+                alt='IW_Stat'
+                className='IW-button'
+                onClick={() => setState(type, false, true)}
+                key={index}
+              ></img>
+            );
+        })}
+      </div>
+      {currentType === 'Description' && <IWDescription hero={hero.replace(/-/g, ' ')} />}
+      {currentType === 'Stat' && <IWStat />}
+    </div>
+  );
+};
 
 export default IWContainer;
-
