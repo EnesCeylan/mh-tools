db8cbbd67a0cfa63840f92dd34794cc7857cf3d1
Enes <aenesceylan98@gmail.com>
2022-01-13 17:35:08 +0300

    Updates

---
+++ static/js/934.c65aee83.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmythic_companion=self.webpackChunkmythic_companion||[]).push([[934],{934:function(e,i,s){s.r(i);var a=s(184);i.default=function(e){var i=e.hero,s=e.heroname;return(0,a.jsxs)("div",{className:"tierlist-hero-box",children:[(0,a.jsx)("img",{alt:"",className:"tierlist-hero-image "+i,src:"/mythic-companion/assets/hero/hero-cards/"+i+".png",loading:"lazy"}),(0,a.jsx)("div",{className:"tierlist-hero-name-container",children:(0,a.jsx)("div",{className:"tierlist-hero-name",children:s})})]})}}}]);
+//# sourceMappingURL=934.c65aee83.chunk.js.map
